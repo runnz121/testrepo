bf4b9b799728738006ca94f2c40a4d025e18b3b1
runnz121 <runnz121@gmail.com>
2022-03-09 04:51:06 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState();
 
-  const [data, setData] = useState()
+  useEffect(() => {
+    const getData = async () => {
+      const getdata = await axios.get("http://3.35.179.18:8080/");
+      console.log(getdata.data);
+      setData(getdata.data);
+    };
+    getData();
+  }, []);
 
-  useEffect( async() => {const getdata = await axios.get("http://3.35.179.18:8080/")
-  console.log(getdata.data)
-  setData(getdata.data)
-},[])
-
-  return (
-
-
-    <div>{data}</div>
-  )
+  return <div>{data}</div>;
 }
 
-export default App
+export default App;
+
