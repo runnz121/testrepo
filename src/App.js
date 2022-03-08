d7cb5eb8e1b7bf94ea0faab91f85c2c032498213
runnz121 <runnz121@gmail.com>
2022-03-09 04:43:53 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const [data, setData] = useState()
+
+  useEffect( async() => {const getdata = await axios.get("http://3.35.179.18:8080/")
+  console.log(getdata.data)
+  setData(getdata.data)
+},[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    <div>{data}</div>
+  )
 }
 
-export default App;
+export default App
